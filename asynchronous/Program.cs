43955079556a56c4581f96fae8f981ca3cb18a88
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-17 17:51:06 +0530

    Add files via upload

---
+++ asynchronous/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynchronous_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program execution begins");
+            Method1();
+            for(int i=0; i<5; i++)
+            {
+                Console.WriteLine("for loop:"+i);
+            }
+        }
+        private static async void Method1()
+        {
+             int value =await Method2();
+            Console.WriteLine("output of Method2 :" + value);
+        }
+        private static Task<int> Method2()
+        {
+            //Thread.Sleep(5000);
+            //return 1;
+            return Task.Run(() =>
+            { 
+                Thread.Sleep(5000); 
+                return 1; 
+            });
+        }
+    }
+}
